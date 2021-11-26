9270b39d237dd51a4e0cf2ad1b17413cfdfa3403
thomas <chardonnens.thomas@protonmail.com>
2021-11-26 21:30:29 +0100

    First Push

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.example.yokai {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.yokai to javafx.fxml;
+    exports com.example.yokai;
+}
