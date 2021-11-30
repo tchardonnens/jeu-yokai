abd1f5e5d275064b9b3250caae70457b26390fa8
thomas <chardonnens.thomas@protonmail.com>
2021-11-30 10:06:36 +0100

    Working for M1

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,11 +3,6 @@ module com.example.yokai {
     requires javafx.fxml;
     requires javafx.web;
 
-    requires org.controlsfx.controls;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-
     opens com.example.yokai to javafx.fxml;
     exports com.example.yokai;
 }
